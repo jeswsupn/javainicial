0bb9dfbb5f06870172af219fcd7f17059e318c48
Docente Evolucion Jesws <jesws.rodriguez@upn.pev2>
2021-02-15 20:35:34 -0500

    Probando segunda version

--- src/main/java/projecto_vacio/Main.java
+++ src/main/java/projecto_vacio/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Probando un texto");
+        System.out.println( "Efectuando segunda prueba");
     }
     
 }
