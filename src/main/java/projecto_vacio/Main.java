5e051a9c853ab3702d11c679f00d559bee718f0c
Docente Evolucion Jesws <jesws.rodriguez@upn.pev2>
2021-02-15 20:44:34 -0500

    Tercera version se mostro otras cosas.

--- src/main/java/projecto_vacio/Main.java
+++ src/main/java/projecto_vacio/Main.java
@@ -16,8 +16,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Probando un texto");
-        System.out.println( "Efectuando segunda prueba");
+        System.out.println("Probando un texto2");
+        //hola
+        System.out.println("Viendo la version 03");
     }
     
 }
